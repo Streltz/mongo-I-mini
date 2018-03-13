018abb03d9ec38b52c22da3408c9c88d55aa5ae4
Mike Streltsoff <MStreltsoff@gmail.com>
2018-03-13 01:02:01 -0400

    Mongo mini complete

--- controllers/BearRoutes.js
+++ controllers/BearRoutes.js
@@ -8,62 +8,97 @@ const STATUS_SEVER_ERROR = 500;
 const STATUS_CREATED_SUCCESS = 201;
 const STATUS_OK = 200;
 
-
 bearRouter.post('/bears', (req, res) => {
-  if( req.body.species &&  req.body.latinName) {
+  if (req.body.species && req.body.latinName) {
     const bear = new Bear(req.body);
-
     bear
-    .save()
-    .then(savedBear => {
-      res.status(STATUS_CREATED_SUCCESS).json(savedBear);
-    })
-    .catch(err => {
-      res.status(STATUS_SEVER_ERROR).send({ error: "There was an error while saving the Bear to the Database" })
-    });
-  } else {
-    res.status(STATUS_BAD_REQUEST).json({ errorMessage: "Please provide both species and latinName for the Bear." });
-  }
-})
-
-bearRouter.get('/bears', (req, res) => {
-    Bear.find({})
-      .then(bears => {
-        res.status(200).json(bears);
+      .save()
+      .then((savedBear) => {
+        res.status(STATUS_CREATED_SUCCESS).json(savedBear);
       })
-      .catch(err => {
-        res.status(500).json({ error: "The information could not be retrieved." });
+      .catch((err) => {
+        res.status(STATUS_SEVER_ERROR).send({
+          error: 'There was an error while saving the Bear to the Database',
+        });
       });
-  });
-
-bearRouter.get('/bears/:id', (req, res) => {
-  Bear.findById(req.params.id)
-    .then(id => {
-      res.status(200).json(id);
+  } else {
+    res.status(STATUS_BAD_REQUEST).json({
+      errorMessage: 'Please provide both species and latinName for the Bear.',
+    });
+  }
+});
+bearRouter.get('/bears', (req, res) => {
+  Bear.find({})
+    .then((bears) => {
+      res.status(STATUS_OK).json(bears);
     })
-      .catch(err => {
-        res.status(404).json({ message: "The Bear with the specified ID does not exist." })
-      })
-  })
-
+    .catch((err) => {
+      res
+        .status(STATUS_SEVER_ERROR)
+        .json({ error: 'The information could not be retrieved.' });
+    });
+});
+bearRouter.get('/bears/:id', (req, res) => {
+  // if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+  Bear.findById(req.params.id)
+    .then((id) => {
+      console.log(id);
+      if (id === null) {
+        res.status(STATUS_NOT_FOUND).json({
+          message: 'The Bear with the specified ID does not exist.',
+        });
+      } else {
+        res.status(STATUS_OK).json(id);
+      }
+    })
+    .catch((err) => {
+      switch (err.name) {
+        case 'CastError':
+          return res
+            .status(STATUS_SEVER_ERROR)
+            .json({ error: 'The information could not be retrieved.' });
+        default:
+          res.status(STATUS_SEVER_ERROR).json({ error: err.name });
+      }
+    });
+  // }
+});
 
 bearRouter.delete('/bears/:id', (req, res) => {
   Bear.findByIdAndRemove(req.params.id)
-  .then(id => {
-    res.status(200).json(bears);
-  })
-  .catch(err => {
-    res.status(404).json({ message: "The Bear with the specified ID does not exist." })
-})
-
+    .then((id) => {
+      res
+        .status(STATUS_OK)
+        .json({ success: `Terrible bear, ${req.params.id},  deleted` });
+    })
+    .catch((err) => {
+      res
+        .status(STATUS_NOT_FOUND)
+        .json({ message: 'The Bear with the specified ID does not exist.' });
+    });
+});
 bearRouter.put('/bears/:id', (req, res) => {
-  Bear.findByIdAndUpdate(req.params.id)
-  .save(id => {
-    res.status(200)
-  })
-  .then
-
-  
-})
+  Bear.findByIdAndUpdate(req.params.id, req.body)
+    .then((bear) => {
+      if (bear === null) {
+        res
+          .status(STATUS_NOT_FOUND)
+          .json({ message: 'The Bear with the specified ID does not exist.' });
+      } else {
+        res
+          .status(STATUS_OK)
+          .json({ success: `Bear, ${req.params.id} Updated` });
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      switch (err.name) {
+        default:
+          res
+            .status(STATUS_SEVER_ERROR)
+            .json({ error: 'The Bear information could not be modified.' });
+      }
+    });
+});
 
 module.exports = bearRouter;
